89823a1bd25757679e098865ad7641338294e3d3
chikai.ito <chikai.ito37@gmail.com>
2020-03-20 02:48:54 +0900

    ロリハをちゃんと整備

--- enjapma/main-library/RollingHash.cpp
+++ enjapma/main-library/RollingHash.cpp
@@ -3,6 +3,8 @@ vector<ll> base_table = {128ll, 243ll, 125ll,
 vector<ll> mod_table =  {2000000011ll, 2000000033ll, 2000000063ll,
                          2147467057ll, 2147483353ll, 3000001811ll};
 
+bool use_table[10];
+
 struct LLRollingHash {
 // long long に対するRollingHash
 public:
@@ -13,9 +15,14 @@ public:
     vector<ll> base_pow;
 
     LLRollingHash(vector<ll> v, ll my_base, ll my_mod) {
+        ll idx = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+        while(use_table[idx]){
+            idx = (idx + 1) % 6;
+        }
+        use_table[idx] = true;
         n = v.size();
-        base = my_base;
-        mod = my_mod;
+        base = base_table[idx];
+        mod = mod_table[idx];
         hash_table.resize(n + 1, 0ll);
         base_pow.resize(n + 1, 1ll);
         ll tmp = 0;
